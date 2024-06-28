129fc0b1775c39c4af802fd3bb7422f5f7c1b17a
rodhis <rodrigonoliva@gmail.com>
2024-06-28 16:14:41 -0300

    first commit

---
+++ readme.md
@@ -0,0 +1,23 @@
+# Calculadora de quantidades para churrascos
+
+Tem dúvidas sobre a quantidade de carne, calabresa, cerveja e outras coisas que vai precisar para seu churrasco? Com essa calculadora seus problemas se acabaram!
+
+<hr />
+
+## Uso
+
+1- Insira a quantidade de pessoas que frequentarão o churrasco. Precisa ter ao menos 1 adulto (homem ou mulher).
+
+2- Clique em calcular.
+
+3- As quantidades de carne bovina, frango, linguiça calabresa, cerveja e refrigerante aparecerão abaixo!
+
+As quantidades seguem os pesos da tabela abaixo:
+
+![tabela de quantidades e pesos](./src/assets/images/quantities-weights.png)
+
+<hr />
+
+### Nota
+
+Esta é a atividade MAPA (Material de Avaliação Prática de Aprendizagem) da disciplina Programação Front-End, para meu curso superior de tecnologia em Sistemas para Internet, na Unicesumar.
---
+++ src/scripts/index.js
@@ -0,0 +1,58 @@
+const calcButton = document.querySelector("#calculate")
+
+calcButton.addEventListener("click", (ev) => {
+    ev.preventDefault()
+
+    const men = Number(document.querySelector("#men").value)
+    const women = Number(document.querySelector("#women").value)
+    const children = Number(document.querySelector("#children").value)
+    const result = document.querySelector("#result")
+    const footerMsg = document.querySelector("#footer")
+
+    let meat
+    let chicken
+    let sausage
+    let beer
+    let soda
+
+    if (men == 0 && women == 0 && children == 0) {
+        result.innerHTML = `<p>Por favor, insira valores válidos.</p>`
+        return
+    }
+
+    if (men > 0) {
+        meat = 0.5 * men
+        chicken = 0.2 * men
+        sausage = 0.2 * men
+        soda = 0.3 * men
+        beer = 0.8 * men
+    }
+
+    if (women > 0) {
+        meat += 0.3 * women
+        chicken += 0.2 * women
+        sausage += 0.2 * women
+        soda += 0.4 * women
+        beer += 0.5 * women
+    }
+
+    if (children > 0) {
+        meat += 0.2 * children
+        chicken += 0.1 * children
+        sausage += 0.2 * children
+        soda += 0.2 * children
+        beer += 0 * children
+    }
+
+    result.innerHTML = `
+    <ul>
+      <li><span>${meat.toFixed(1)}kg</span> de carne bovina,</li>
+      <li><span>${chicken.toFixed(1)}kg</span> de frango,</li>
+      <li><span>${sausage.toFixed(1)}kg</span> de linguiça,</li>
+      <li><span>${soda.toFixed(1)}L</span> de refrigerante,</li>
+      <li><span>${beer.toFixed(1)}L</span> de cerveja.</li>
+    </ul>
+  `
+    footerMsg.style.visibility = "visible"
+    return
+})
